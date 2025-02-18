3da8f1ad50776a6f71094974f5d768e18acb12f5
Peugh18 <miguelurcia2004@gmail.com>
2025-02-17 20:57:21 -0500

    FALTA AGREGAR GOOGLE SHEETS

--- src/services/messageHandler.js
+++ src/services/messageHandler.js
@@ -1,18 +1,28 @@
 import whatsappService from './whatsappService.js';
 
 class MessageHandler {
+  constructor() {
+    this.appointmenState = {};
+  }
+
   async handleIncomingMessage(message, senderInfo) {
     if (message?.type === 'text') {
       const incomingMessage = message.text.body.toLowerCase().trim();
 
-      if(this.isGreeting(incomingMessage)){
+      if (this.isGreeting(incomingMessage)) {
         await this.sendWelcomeMessage(message.from, message.id, senderInfo);
         await this.sendWelcomeMenu(message.from);
-      } else if(incomingMessage === 'media') {
+      } else if (incomingMessage === 'media') {
         await this.sendMedia(message.from);
       } else {
-        const response = `Echo: ${message.text.body}`;
-        await whatsappService.sendMessage(message.from, response, message.id);
+        const state = this.appointmenState[message.from];
+        if (state) {
+          // Continuar flujo de compra si el estado está activo
+          await this.handleAppointmentFlow(message.from, incomingMessage);
+        } else {
+          const response = `Echo: ${message.text.body}`;
+          await whatsappService.sendMessage(message.from, response, message.id);
+        }
       }
       await whatsappService.markAsRead(message.id);
     } else if (message?.type === 'interactive') {
@@ -23,7 +33,7 @@ class MessageHandler {
   }
 
   isGreeting(message) {
-    const greetings = ["hola", "hello", "hi", "buenas tardes", "buenos días", "buenas noches","¡Hola, quiero más información sobre sus productos!"];
+    const greetings = ["hola", "hello", "hi", "buenas tardes", "buenos días", "buenas noches", "¡Hola, quiero más información sobre sus productos!"];
     return greetings.includes(message);
   }
 
@@ -33,64 +43,158 @@ class MessageHandler {
 
   async sendWelcomeMessage(to, messageId, senderInfo) {
     const name = this.getSenderName(senderInfo);
-    const welcomeMessage = `Hola ${name}, Bienvenido a ShopLife, tu tienda de accesorios y más. ¿En qué puedo ayudarte hoy?`;
+    const welcomeMessage = `Hola ${name}, Bienvenido a UP STORE, tu tienda de accesorios y más. ¿En qué puedo ayudarte hoy?`;
     await whatsappService.sendMessage(to, welcomeMessage, messageId);
   }
 
   async sendWelcomeMenu(to) {
-    const menuMessage = "Elige una Opción"
+    const menuMessage = "Elige una Opción";
     const buttons = [
-      {
-        type: 'reply', reply: { id: 'option_1', title: 'Ver Productos' }
-      },
-      { 
-        type: 'reply', reply: { id: 'option_2', title: 'Promociones'}
-      },
-      {
-        type: 'reply', reply: { id: 'option_3', title: 'Hablar con un Ases.'}
-      }
+      { type: 'reply', reply: { id: 'option_1', title: 'Ver Productos' } },
+      { type: 'reply', reply: { id: 'option_2', title: 'Promociones' } },
+      { type: 'reply', reply: { id: 'option_3', title: 'Hablar con un Asesor' } }
     ];
+    await whatsappService.sendInteractiveButtons(to, menuMessage, buttons);
+  }
 
+  async sendAdditionalOptionsMenu(to) {
+    const menuMessage = "¿Qué te gustaría hacer ahora?";
+    const buttons = [
+      { type: 'reply', reply: { id: 'option_4', title: 'Comprar Producto' } },
+      { type: 'reply', reply: { id: 'option_5', title: 'Contactar Asesor' } }
+    ];
     await whatsappService.sendInteractiveButtons(to, menuMessage, buttons);
   }
 
   async handleMenuOption(to, option) {
-    console.log(option)
     let response;
+
     switch (option) {
       case 'ver productos':
-        response = "Estos son nuestros productos";
+        await this.sendMedia(to, 'document', 'https://drive.google.com/uc?export=download&id=1SZ8gDr7dWofGlt-1f6osr_MZmbnRMhiP', '¡Aquí están los productos que tenemos en este año 2025!');
+        await this.sendAdditionalOptionsMenu(to);
         break;
+
       case 'promociones':
-        response = "Estas son nuestras promociones";
-        break
-      case 'hablar con un ases.': 
-       response = 'Un asesor se pondrá en contacto contigo en breve.';
-       break
-      default: 
-       response = "Lo siento, no entendí tu selección, Por Favor, elige una de las opciones del menú."
+        response = "Nuestras promociones están agotadas por el momento. ¡Pero no te preocupes! Estamos trabajando en nuevas promociones para ti. 😊";
+        break;
+
+      case 'hablar con un asesor':
+        response = 'Un asesor se pondrá en contacto contigo en breve.';
+        break;
+
+      case 'comprar producto':
+        this.appointmenState[to] = { step: 'name' };
+        response = "Indícanos tu nombre para continuar con la compra.";
+        break;
+
+      case 'contactar asesor':
+        response = `Gracias por tu interés. Un asesor se pondrá en contacto contigo lo antes posible. Si deseas acelerar el proceso, por favor proporciona más detalles sobre tu consulta. 😊`;
+        break;
+
+      default:
+        response = "Lo siento, no entendí tu selección. Por favor, elige una de las opciones del menú.";
     }
+
+    if (response) {
+      await whatsappService.sendMessage(to, response);
+    }
+  }
+  completeAppointment(to) {
+    const appointment = this.appointmenState[to];
+    if (!appointment) {
+      return console.log("No se encontró ninguna cita activa para este número.");
+    }
+  
+    // Registro de datos del usuario
+    const userData = [
+      to,
+      appointment.name,
+      appointment.celular,
+      appointment.direccion,
+      new Date().toISOString()
+    ];
+    console.log('Datos del usuario:', userData);
+  
+    // Simula guardado en la base de datos
+    this.saveAppointmentToDatabase(appointment);
+  
+    // Eliminar el estado de la cita para este usuario
+    delete this.appointmenState[to];
+  
+    // Mensaje de confirmación final al usuario
+    const completionMessage = `¡Gracias por confirmar tu pedido, ${appointment.name}! Procederemos a procesar tu pedido y te enviaremos una confirmación por este medio pronto. 😊`;
+    whatsappService.sendMessage(to, completionMessage);
+  }
+  
+  async handleAppointmentFlow(to, message) {
+    if (!this.appointmenState[to]) {
+      this.appointmenState[to] = { step: 'name' };
+    }
+
+    const state = this.appointmenState[to];
+    let response;
+
+    switch (state.step) {
+      case 'name':
+        state.name = message;
+        state.step = 'celular';
+        response = 'Por favor, indícame tu número de celular para continuar con la programación del envío.';
+        break;
+
+      case 'celular':
+        state.celular = message;
+        state.step = 'direccion';
+        response = 'Perfecto. Ahora, por favor indícame tu dirección de envío.';
+        break;
+
+      case 'direccion':
+        state.direccion = message;
+        state.step = 'confirmar';
+        response = `¡Gracias! Hemos recibido tu información:\n
+        - Nombre: ${state.name}\n
+        - Celular: ${state.celular}\n
+        - Dirección: ${state.direccion}\n
+        ¿Es correcto? Responde con "Sí" para confirmar o "No" para reiniciar.`;
+        break;
+
+      case 'confirmar':
+        if (message.toLowerCase() === 'si') {
+          response = this.completeAppointment(to);
+          this.saveAppointmentToDatabase(state);
+          delete this.appointmenState[to];
+          await this.sendPaymentMethod(to);
+        } else if (message.toLowerCase() === 'no') {
+          response = 'Entiendo. Vamos a reiniciar el proceso. Por favor, indícame tu nombre.';
+          state.step = 'name';
+        } else {
+          response = 'Por favor responde con "Si" o "No".';
+        }
+        break;
+
+      default:
+        response = 'Lo siento, no entendí tu mensaje. Por favor, indícame tu nombre para continuar.';
+        state.step = 'name';
+    }
+
     await whatsappService.sendMessage(to, response);
   }
+  async sendPaymentMethod(to) {
+    const paymentMessage = `¡Gracias por elegir ShopLife! Para realizar el pago por Yape, utiliza los siguientes datos:
 
-  async sendMedia(to) {
-    // const mediaUrl = 'https://s3.amazonaws.com/gndx.dev/medpet-audio.aac';
-    // const caption = 'Bienvenida';
-    // const type = 'audio';
+👉 Número de Yape: *959166911*  
+👉 Nombre: *José Urcia*
 
-    // const mediaUrl = 'https://s3.amazonaws.com/gndx.dev/medpet-imagen.png';
-    // const caption = '¡Esto es una Imagen!';
-    // const type = 'image';
+Una vez realizado el pago, envíanos una captura para confirmar tu pedido. ¡Agradecemos tu preferencia! 😊`;
+    await whatsappService.sendMessage(to, paymentMessage);
+  }
 
-    // const mediaUrl = 'https://s3.amazonaws.com/gndx.dev/medpet-video.mp4';
-    // const caption = '¡Esto es una video!';
-    // const type = 'video';
-
-    const mediaUrl = 'https://drive.google.com/uc?export=download&id=1SZ8gDr7dWofGlt-1f6osr_MZmbnRMhiP';
-    const caption = '¡Esto es un PDF!';
-    const type = 'document';
-    
+  async sendMedia(to, type, mediaUrl, caption) {
     await whatsappService.sendMediaMessage(to, type, mediaUrl, caption);
+  }
+
+  saveAppointmentToDatabase(state) {
+    console.log("Datos guardados en la base de datos:", state);
   }
 }
 
--- src/services/whatsappService.js
+++ src/services/whatsappService.js
@@ -4,6 +4,13 @@ import config from '../config/env.js';
 class WhatsAppService {
   async sendMessage(to, body, messageId) {
     try {
+      // Verificamos que 'body' no esté vacío
+      if (!body) {
+        console.error('El cuerpo del mensaje no puede estar vacío');
+        return;
+      }
+
+      // Enviar mensaje de texto
       await axios({
         method: 'POST',
         url: `https://graph.facebook.com/${config.API_VERSION}/${config.BUSINESS_PHONE}/messages`,
@@ -13,19 +20,27 @@ class WhatsAppService {
         data: {
           messaging_product: 'whatsapp',
           to,
-          text: { body },
+          text: { body }, // Aquí aseguramos que 'body' sea un objeto con el texto
+          // Se descomenta el 'context' si necesitas usar el ID del mensaje
           // context: {
           //   message_id: messageId,
           // },
         },
       });
+      console.log('Mensaje enviado con éxito');
     } catch (error) {
-      console.error('Error sending message:', error);
+      console.error('Error al enviar el mensaje:', error.response ? error.response.data : error.message);
     }
   }
 
   async markAsRead(messageId) {
     try {
+      if (!messageId) {
+        console.error('El ID del mensaje no puede estar vacío');
+        return;
+      }
+
+      // Marcar el mensaje como leído
       await axios({
         method: 'POST',
         url: `https://graph.facebook.com/${config.API_VERSION}/${config.BUSINESS_PHONE}/messages`,
@@ -38,13 +53,20 @@ class WhatsAppService {
           message_id: messageId,
         },
       });
+      console.log('Mensaje marcado como leído');
     } catch (error) {
-      console.error('Error marking message as read:', error);
+      console.error('Error al marcar el mensaje como leído:', error.response ? error.response.data : error.message);
     }
   }
 
-  async sendInteractiveButtons(to, BodyText, buttons) {
+  async sendInteractiveButtons(to, bodyText, buttons) {
     try {
+      if (!bodyText || !buttons || buttons.length === 0) {
+        console.error('El texto del mensaje y los botones son requeridos');
+        return;
+      }
+
+      // Enviar botones interactivos
       await axios({
         method: 'POST',
         url: `https://graph.facebook.com/${config.API_VERSION}/${config.BUSINESS_PHONE}/messages`,
@@ -57,39 +79,47 @@ class WhatsAppService {
           type: 'interactive',
           interactive: {
             type: 'button',
-            body: { text: BodyText },
+            body: { text: bodyText },
             action: {
-              buttons: buttons
-            }
-          }
+              buttons,
+            },
+          },
         },
       });
+      console.log('Mensaje interactivo enviado con éxito');
     } catch (error) {
-      console.error(error);
+      console.error('Error al enviar el mensaje interactivo:', error.response ? error.response.data : error.message);
     }
   }
 
   async sendMediaMessage(to, type, mediaUrl, caption) {
     try {
+      if (!mediaUrl || !type) {
+        console.error('El tipo de medio y la URL son requeridos');
+        return;
+      }
+
       const mediaObject = {};
 
       switch (type) {
         case 'image':
-          mediaObject.image = { link: mediaUrl, caption: caption }
+          mediaObject.image = { link: mediaUrl, caption };
           break;
         case 'audio':
-          mediaObject.audio = { link:mediaUrl }
+          mediaObject.audio = { link: mediaUrl };
           break;
         case 'video':
-          mediaObject.video = { link: mediaUrl, caption: caption }
+          mediaObject.video = { link: mediaUrl, caption };
           break;
         case 'document':
-          mediaObject.document = { link: mediaUrl, caption: caption, filename: 'productos.pdf' }
+          mediaObject.document = { link: mediaUrl, caption, filename: 'productos.pdf' };
           break;
         default:
-          throw new Error('Not Soported Media Type');
+          console.error('Tipo de medio no soportado');
+          return;
       }
 
+      // Enviar mensaje multimedia
       await axios({
         method: 'POST',
         url: `https://graph.facebook.com/${config.API_VERSION}/${config.BUSINESS_PHONE}/messages`,
@@ -99,12 +129,13 @@ class WhatsAppService {
         data: {
           messaging_product: 'whatsapp',
           to,
-          type: type,
-          ...mediaObject
+          type,
+          ...mediaObject, // Desestructuramos el objeto de medios
         },
       });
+      console.log('Mensaje multimedia enviado con éxito');
     } catch (error) {
-      console.error('Error sending Media', error);
+      console.error('Error al enviar el mensaje multimedia:', error.response ? error.response.data : error.message);
     }
   }
 }
